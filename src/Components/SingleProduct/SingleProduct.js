8397e3baeeec3b0f599c87243601b8dfaf668899
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-03-21 20:30:49 +0530

    refactor: added singleproduct card

---
+++ src/Components/SingleProduct/SingleProduct.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function SingleProduct({ product: {_id, img, productName, bestSeller, price, originalprice} }){
+
+
+  let navigate = useNavigate()
+  
+  return (
+    <div className="card">
+        <div classNameName="card__image--container badge-content">
+            <img onClick={()=> navigate('/product/'+ _id)} className="card__image" src={img} />
+            {bestSeller ? <p className="badge badge--large badge-card-tr">Best Seller</p> : null}
+        </div>
+        <div className="card__body padding--medium">
+            <div className="card__heading">
+                <div className="container__flex--spacebetween">
+                    <h2 className="card__title text--large">{productName}</h2>
+                    <i className="far fa-heart text--large"></i>
+                </div>
+            </div> 
+            <div className="card__description margin-bottom--small">
+                <p className="clr--secondary text--bold">Rs. {price} <span className="text--line-through"> {originalprice}</span></p>
+            </div>
+            <div className="card__actions">
+                <div className="card__actions--buttons">
+                   <button className="btn btn--secondary btn--icon cart"><i className="fas fa-shopping-cart"></i>Add to Cart</button>              
+                </div>
+            </div>
+        </div> 
+    </div>
+  )
+}
+
+export default SingleProduct
