c13401b5725789472a75a35e23ad12ca2862eaca
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-03-21 21:16:16 +0530

    bug : class name fixed

--- src/Components/Loader/LoaderOverlay.js
+++ src/Components/Loader/LoaderOverlay.js
@@ -3,10 +3,10 @@ import React from 'react'
 function LoaderOverlay() {
   return (
     <>
-        <div classNameName="loader loader--overlay center">
+        <div className="loader loader--overlay center">
             <div className="sp sp-circle"></div>
         </div>
-        <div classNameName="loader__container--overlay">
+        <div className="loader__container--overlay">
 
         </div>
     </>
