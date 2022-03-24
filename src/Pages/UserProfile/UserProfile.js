3a762c6db2d9156e24534947ac95e68446e600c2
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-03-24 10:45:08 +0530

    feat: added user profile page

--- src/Pages/UserProfile/UserProfile.js
+++ src/Pages/UserProfile/UserProfile.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react';
+import './UserProfile.css'
 
 function UserProfile() {
+  const [editmode, seteditmode] = useState(false)
+
   return (
-    <div>UserProfile</div>
+    <div className="user-profile__container">
+        <h2 className="text--center margin-tb--medium">User Profile</h2>
+        <div className="form">
+
+            <div className="form-element">
+                <label className="form-label form-label--required">User Name</label>
+                <input type="text" className="form-field" value="efe" placeholder="Username" disabled={!editmode} />
+            </div>
+            
+            <div className="form-element">
+                <label className="form-label form-label--required">First Name</label>
+                <input type="text" className="form-field" value="efe" placeholder="Enter First Name" disabled={!editmode} />
+            </div>
+            
+            <div className="form-element">
+                <label className="form-label form-label--required">Last Name</label>
+                <input type="text" className="form-field" placeholder="Enter Last Name" disabled={!editmode} />
+            </div>
+
+            <div className="form-element">
+                <label className="form-label form-label--required">Email</label>
+                <input type="email" className="form-field--text-only" value="abc@example.com" disabled={!editmode} />
+            </div>
+
+            <div className="form-element">
+                <label className="form-label form-label--required">Phone No.</label>
+                <input type="Number" className="form-field" value="98765656556" disabled={!editmode} />
+            </div>
+
+            <div className="form-element">
+                <label className="form-label">Gender</label>
+
+                <input type="radio" className="form-radio" name="gender" value="Male" disabled={!editmode} />
+                <span> Male </span>
+                <input type="radio" className="form-radio" name="gender" value="Female" disabled={!editmode} />
+                <span> Female</span>
+            </div>
+            {editmode ? <button onClick={()=> seteditmode(false)} className="btn btn--outline-secondary margin--small">Save</button> : 
+                        <button onClick={()=> seteditmode(true)}className="btn btn--outline-secondary margin--small">Edit</button> }
+            
+        </div>
+    </div>
   )
 }
 
