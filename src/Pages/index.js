e62afbcd6609b82df03728770adeec58054119ad
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-04-06 18:15:25 +0530

    bug: name fix

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,7 +1,7 @@
 export { HomePage } from './HomePage/HomePage';
 export { ProductListingPage } from './ProductListingPage/ProductListingPage';
 export { SingleProductPage } from './SingleProductPage/SingleProductPage';
-export { CartManagementPage } from './CartManagementPage/CartManagementPage';
+export { CartManagementPage } from './CartManagementPage/CartManagementpage';
 export { WishlistPage } from './WishlistPage/WishlistPage';
 export { UserProfile } from './UserProfile/UserProfile';
 export { Login } from './Login/Login';
