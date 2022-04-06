8f381d933e2a92c9e391bbcf16b0cf14b9468f03
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-04-06 11:04:10 +0530

    refactor: removed unnecessary code

--- src/Context/AuthContext/AuthContext.js
+++ src/Context/AuthContext/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext } from "react";
 import { useAuth } from "../../hooks/useAuth";
 
 const AuthContext = createContext()
