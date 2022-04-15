7ab7235f0036773bacaf611db02fd0ebf3b2f0bf
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-04-15 10:35:07 +0530

    refactor: code review changes

--- src/Components/UserReview/UserReview.js
+++ src/Components/UserReview/UserReview.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './UserReview.css'
 
 function UserReview({ userReview: {userID, userProfile, review, ratings}}) {
   return (
-    <div className="container__flex--center margin-tb--medium">
+    <div className="container__flex--center margin-tb--medium container--relative">
         <div className="user__profile--container">
             <img className="user__profile--image" src={userProfile} />
         </div>
         <p className="margin-left--medium"> <span className="para-rating__container">{ratings} &#9733;</span> {review} </p>
+        <i className="fas fa-ellipsis-v"></i>
+        <div className='review--actions'>
+          <p>Edit</p>
+          <p>Delete</p>
+        </div>
     </div>
   )
 }
